586dcb4e888cc069d6a0408f72fbde2b65a05f48
BenocxX <mathis.cote@outlook.com>
2022-04-14 21:48:26 -0400

    feat: create template for pug file

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,9 +1,17 @@
 <?php namespace Controllers;
 
+use Zephyrus\Network\Response;
+
 class HomeController extends Controller
 {
     public function initializeRoutes()
     {
+        $this->get("/", "home");
+        $this->get("/home", "home");
+    }
 
+    public function home(): Response
+    {
+        return $this->render("home");
     }
 }
