18daa66d2d4ce2aeee06d399ea0acadfe3a80d3c
BenocxX <mathis.cote@outlook.com>
2022-04-14 21:57:11 -0400

    feat: add default response for main pages

--- app/Controllers/AboutController.php
+++ app/Controllers/AboutController.php
@@ -1,9 +1,16 @@
 <?php namespace Controllers;
 
+use Zephyrus\Network\Response;
+
 class AboutController extends Controller
 {
     public function initializeRoutes()
     {
+        $this->get("/about", "about");
+    }
 
+    public function about(): Response
+    {
+        return $this->render("about");
     }
 }
--- app/Controllers/RequestController.php
+++ app/Controllers/RequestController.php
@@ -1,9 +1,16 @@
 <?php namespace Controllers;
 
+use Zephyrus\Network\Response;
+
 class RequestController extends Controller
 {
     public function initializeRoutes()
     {
+        $this->get("/request", "request");
+    }
 
+    public function request(): Response
+    {
+        return $this->render("request");
     }
 }
